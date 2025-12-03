6aef1c2856448a28acbb57f4b5c8f67150b4ad5c
Benstone Zhang <benstonezhang@gmail.com>
2025-12-03 18:30:03 +0800

    refine qwen2 and qwen2.5

--- llm/qwen2_5_vl.py
+++ llm/qwen2_5_vl.py
@@ -1,53 +1,45 @@
 import torch
 from typing_extensions import override
 
-from .model import MultiModalModel
+from .qwen_vl import Qwen_VLMultiModalModel, Qwen_VLVisionForOnnx
 
 
-class Qwen2_5_VLVisionForOnnx(torch.nn.Module):
-    merge_size = 2
-    temporal_patch_size = 2
-    patch_size = 14
-    channel = 3
-
-    def __init__(self, vpm, batch_size, height, width):
+class Qwen2_5_VLVisionForOnnx(Qwen_VLVisionForOnnx):
+    def __init__(self, *args, **kwargs):
         import numpy as np
 
-        super().__init__()
-        self.vpm = vpm
-        self.batch_size = batch_size
-        self.grid_t = (batch_size + self.temporal_patch_size - 1) // self.temporal_patch_size
-        self.grid_h = height // self.patch_size
-        self.grid_w = width // self.patch_size
+        super().__init__(*args, **kwargs)
         grid_thw = np.array([[self.grid_t, self.grid_h, self.grid_w]])
 
         def forward(pixel_values, **kwargs):
-            hidden_states = vpm.patch_embed(pixel_values)
-            rotary_pos_emb = vpm.rot_pos_emb(grid_thw)
-            window_index, cu_window_seqlens = vpm.get_window_index(grid_thw)
+            hidden_states = self.vpm.patch_embed(pixel_values)
+            rotary_pos_emb = self.vpm.rot_pos_emb(grid_thw)
+            window_index, cu_window_seqlens = self.vpm.get_window_index(grid_thw)
             cu_window_seqlens = torch.tensor(cu_window_seqlens, device=hidden_states.device, dtype=torch.int32)
             cu_window_seqlens = torch.unique_consecutive(cu_window_seqlens)
 
             seq_len, _ = hidden_states.size()
-            hidden_states = hidden_states.reshape(seq_len // vpm.spatial_merge_unit, vpm.spatial_merge_unit, -1)
+            hidden_states = hidden_states.reshape(seq_len // self.vpm.spatial_merge_unit,
+                                                  self.vpm.spatial_merge_unit, -1)
             hidden_states = hidden_states[window_index, :, :]
             hidden_states = hidden_states.reshape(seq_len, -1)
-            rotary_pos_emb = rotary_pos_emb.reshape(seq_len // vpm.spatial_merge_unit, vpm.spatial_merge_unit, -1)
+            rotary_pos_emb = rotary_pos_emb.reshape(seq_len // self.vpm.spatial_merge_unit,
+                                                    self.vpm.spatial_merge_unit, -1)
             rotary_pos_emb = rotary_pos_emb[window_index, :, :]
             rotary_pos_emb = rotary_pos_emb.reshape(seq_len, -1)
             emb = torch.cat((rotary_pos_emb, rotary_pos_emb), dim=-1)
             position_embeddings = (emb.cos(), emb.sin())
 
-            cu_seqlens = torch.repeat_interleave(torch.asarray(self.grid_h * self.grid_w), torch.asarray(self.grid_t)
-                                                 ).cumsum(dim=0, dtype=torch.int32)
+            cu_seqlens = torch.repeat_interleave(torch.asarray(self.grid_h * self.grid_w),
+                                                 torch.asarray(self.grid_t)).cumsum(dim=0, dtype=torch.int32)
             cu_seqlens = torch.nn.functional.pad(cu_seqlens, (1, 0), value=0)
 
-            for layer_num, blk in enumerate(vpm.blocks):
+            for layer_num, blk in enumerate(self.vpm.blocks):
                 hidden_states = blk(hidden_states,
-                                    cu_seqlens=cu_seqlens if layer_num in vpm.fullatt_block_indexes else cu_window_seqlens,
+                                    cu_seqlens=cu_seqlens if layer_num in self.vpm.fullatt_block_indexes else cu_window_seqlens,
                                     position_embeddings=position_embeddings, **kwargs)
 
-            hidden_states = vpm.merger(hidden_states)
+            hidden_states = self.vpm.merger(hidden_states)
             reverse_indices = torch.argsort(window_index)
             hidden_states = hidden_states[reverse_indices, :]
 
@@ -55,38 +47,9 @@ class Qwen2_5_VLVisionForOnnx(torch.nn.Module):
 
         self.vpm.forward = forward
 
-    def forward(self, pixel_values):
-        pixel_values = pixel_values.type(self.vpm.dtype)
-        if self.batch_size == 1:
-            pixel_inputs = pixel_values.repeat(self.temporal_patch_size, 1, 1, 1)
-        elif self.batch_size % self.temporal_patch_size != 0:
-            repeat_time = self.temporal_patch_size - self.batch_size % self.temporal_patch_size
-            repeat_image = pixel_values[-1:, ...].repeat(repeat_time, 1, 1, 1)
-            pixel_inputs = torch.cat((pixel_values, repeat_image), dim=0)
-        else:
-            pixel_inputs = pixel_values
-        patches = pixel_inputs.reshape(self.grid_t, self.temporal_patch_size, self.channel,
-                                       self.grid_h // self.merge_size, self.merge_size, self.patch_size,
-                                       self.grid_w // self.merge_size, self.merge_size, self.patch_size)
-        patches = patches.permute(0, 3, 6, 4, 7, 2, 1, 5, 8)
-        flatten_patches = patches.reshape(self.grid_t * self.grid_h * self.grid_w,
-                                          self.channel * self.temporal_patch_size * self.patch_size * self.patch_size)
 
-        return self.vpm(flatten_patches)
-
-
-# The default range for the number of visual tokens per image in the model is 4-16384.You can set min_pixels and
-# max_pixels according to your needs, such as a token count range of 256-1280, to balance speed and memory usage.
-# min_pixels = 256*28*28
-# max_pixels = 1280*28*28
-# processor = AutoProcessor.from_pretrained("Qwen/Qwen2-VL-2B-Instruct", min_pixels=min_pixels, max_pixels=max_pixels)
-
-class Qwen2_5_VLMultiModalModel(MultiModalModel):
-    vision_mean = [0.48145466 * 255, 0.4578275 * 255, 0.40821073 * 255]
-    vision_std = [0.26862954 * 255, 0.26130258 * 255, 0.27577711 * 255]
-    image_size = 448
-    # image_tokens = '<|vision_start|><|image_pad|>...<|vision_end|>'
-    processor_config = {"use_fast": False, "min_pixels": 256 * 28 * 28, "max_pixels": 2048 * 28 * 28}
+class Qwen2_5_VLMultiModalModel(Qwen_VLMultiModalModel):
+    processor_config = {**Qwen_VLMultiModalModel.processor_config, "use_fast": False}
 
     @override
     @classmethod
@@ -135,32 +98,3 @@ class Qwen2_5_VLMultiModalModel(MultiModalModel):
     #             [batch_size, 3]
     #         ]
     #     }
-
-    @override
-    def get_input_embeddings(self, text_input: str, image_input: str, audio_input=None, audio_input_mask=None):
-        from PIL import Image
-
-        conversation = [
-            {
-                "role": "user",
-                "content": [
-                    {"type": "image"},
-                    {"type": "text", "text": text_input},
-                ],
-            }
-        ]
-
-        # Preprocess the inputs
-        text_prompt = self.processor.apply_chat_template(conversation, add_generation_prompt=True)
-        # Excepted output: '<|im_start|>system\nYou are a helpful assistant.<|im_end|>\n<|im_start|>user\n<|vision_start|><|image_pad|><|vision_end|>Describe this image.<|im_end|>\n<|im_start|>assistant\n'
-        image = Image.open(image_input)
-        inputs = self.processor(text=[text_prompt], images=[image], padding=True, return_tensors="pt").to(
-                self.model.device)
-
-        inputs_embeds = self.embed_tokens(inputs["input_ids"])
-        pixel_values = inputs["pixel_values"].type(self.vision_model.dtype)
-        image_mask = inputs["input_ids"] == self.model.config.image_token_id
-        image_embeds = self.vision_model(pixel_values, grid_thw=inputs["image_grid_thw"]).to(inputs_embeds.device)
-        inputs_embeds[image_mask] = image_embeds
-
-        return inputs_embeds
--- llm/qwen2_vl.py
+++ llm/qwen2_vl.py
@@ -1,25 +1,12 @@
 import torch
 from typing_extensions import override
 
-from .model import MultiModalModel
+from .qwen_vl import Qwen_VLMultiModalModel, Qwen_VLVisionForOnnx
 
 
-class Qwen2_VLVisionForOnnx(torch.nn.Module):
-    merge_size = 2
-    temporal_patch_size = 2
-    patch_size = 14
-    channel = 3
-
-    def __init__(self, vpm, batch_size, height, width):
-        super().__init__()
-        self.vpm = vpm
-        self.batch_size = batch_size
-        self.height = height
-        self.width = width
-        self.grid_t = (batch_size + self.temporal_patch_size - 1) // self.temporal_patch_size
-        self.grid_h = height // self.patch_size
-        self.grid_w = width // self.patch_size
-
+class Qwen2_VLVisionForOnnx(Qwen_VLVisionForOnnx):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
         grid_thw = torch.tensor([[self.grid_t, self.grid_h, self.grid_w]], dtype=torch.int64)
         rotary_pos_emb = self.vpm.rot_pos_emb(grid_thw).cpu().detach().numpy()
         cu_seqlens = torch.repeat_interleave(grid_thw[:, 1] * grid_thw[:, 2],
@@ -38,38 +25,9 @@ class Qwen2_VLVisionForOnnx(torch.nn.Module):
 
         self.vpm.forward = forward
 
-    def forward(self, pixel_values):
-        if self.batch_size == 1:
-            pixel_inputs = pixel_values.repeat(self.temporal_patch_size, 1, 1, 1)
-        elif self.batch_size % self.temporal_patch_size != 0:
-            repeat_time = self.temporal_patch_size - self.batch_size % self.temporal_patch_size
-            repeat_image = pixel_values[-1:, ...].repeat(repeat_time, 1, 1, 1)
-            pixel_inputs = torch.cat((pixel_values, repeat_image), dim=0)
-        else:
-            pixel_inputs = pixel_values
 
-        patches = pixel_inputs.reshape(self.grid_t, self.temporal_patch_size, self.channel,
-                                       self.grid_h // self.merge_size, self.merge_size, self.patch_size,
-                                       self.grid_w // self.merge_size, self.merge_size, self.patch_size)
-        patches = patches.permute(0, 3, 6, 4, 7, 2, 1, 5, 8)
-        flatten_patches = patches.reshape(self.grid_t * self.grid_h * self.grid_w,
-                                          self.channel * self.temporal_patch_size * self.patch_size * self.patch_size)
-
-        return self.vpm(flatten_patches)
-
-
-# The default range for the number of visual tokens per image in the model is 4-16384.You can set min_pixels and
-# max_pixels according to your needs, such as a token count range of 256-1280, to balance speed and memory usage.
-# min_pixels = 256*28*28
-# max_pixels = 1280*28*28
-# processor = AutoProcessor.from_pretrained("Qwen/Qwen2-VL-2B-Instruct", min_pixels=min_pixels, max_pixels=max_pixels)
-
-class Qwen2_VLMultiModalModel(MultiModalModel):
-    vision_mean = [0.48145466 * 255, 0.4578275 * 255, 0.40821073 * 255]
-    vision_std = [0.26862954 * 255, 0.26130258 * 255, 0.27577711 * 255]
-    image_size = 448
-    # image_tokens = '<|vision_start|><|image_pad|>...<|vision_end|>'
-    processor_config = {"use_fast": False, "min_pixels": 256 * 28 * 28, "max_pixels": 2048 * 28 * 28}
+class Qwen2_VLMultiModalModel(Qwen_VLMultiModalModel):
+    processor_config = {**Qwen_VLMultiModalModel.processor_config, "use_fast": False}
 
     @override
     @classmethod
@@ -92,32 +50,3 @@ class Qwen2_VLMultiModalModel(MultiModalModel):
     @override
     def get_vision(self):
         return Qwen2_VLVisionForOnnx(self.vision_model, 1, self.image_size, self.image_size)
-
-    @override
-    def get_input_embeddings(self, text_input: str, image_input: str, audio_input=None, audio_input_mask=None):
-        from PIL import Image
-
-        conversation = [
-            {
-                "role": "user",
-                "content": [
-                    {"type": "image"},
-                    {"type": "text", "text": text_input},
-                ],
-            }
-        ]
-
-        # Preprocess the inputs
-        text_prompt = self.processor.apply_chat_template(conversation, add_generation_prompt=True)
-        # Excepted output: '<|im_start|>system\nYou are a helpful assistant.<|im_end|>\n<|im_start|>user\n<|vision_start|><|image_pad|><|vision_end|>Describe this image.<|im_end|>\n<|im_start|>assistant\n'
-        image = Image.open(image_input)
-        inputs = self.processor(text=[text_prompt], images=[image], padding=True, return_tensors="pt").to(
-                self.model.device)
-
-        inputs_embeds = self.embed_tokens(inputs["input_ids"])
-        pixel_values = inputs["pixel_values"].type(self.vision_model.dtype)
-        image_mask = inputs["input_ids"] == self.model.config.image_token_id
-        image_embeds = self.vision_model(pixel_values, grid_thw=inputs["image_grid_thw"]).to(inputs_embeds.device)
-        inputs_embeds[image_mask] = image_embeds
-
-        return inputs_embeds
